eb0ce75039e4a7aa85cfe1bbf9897d1d57bd7521
devonblackbeard <devon.blackbeard@gmail.com>
2023-12-15 20:01:20 -0700

    Netlify config files for deployment

---
+++ netlify/functions/api.ts
@@ -0,0 +1,15 @@
+// take-home-full-stack/netlify/functions/api.ts
+
+import express, { Router } from "express";
+import serverless from "serverless-http";
+
+const api = express();
+
+const router = Router();
+//router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/search", router)
+
+
+
+export const handler = serverless(api);
