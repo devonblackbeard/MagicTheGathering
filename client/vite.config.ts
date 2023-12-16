0e8beed88fd961864723689294ae3422fb27196b
devonblackbeard <devon.blackbeard@gmail.com>
2023-12-16 00:57:54 -0700

    Ran a build on the client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '../src/index.ts',
+    },
+  },
 })
